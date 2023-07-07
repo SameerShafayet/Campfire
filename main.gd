cfa020fa2c2942229776529a0f25fd5bcdd7e5cc
Sameer <sameershafayet2002@gmail.com>
2023-07-07 11:58:39 +0600

    Menu redesigned

---
+++ main.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/NewGamebutton.grab_focus()
+
+
+
+func _on_new_gamebutton_pressed():
+	get_tree().change_scene()
+
+
+func _on_loadbutton_pressed():
+	get_tree().change_scene
+	
+
+func _on_quitbutton_pressed():
+	get_tree().quit()
+	
+
+
